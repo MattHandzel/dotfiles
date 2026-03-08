aad66eb5b7de8b6f0a03f01a13980d243d9ed9d3
MattHandzel <handzelmatthew@gmail.com>
2026-03-08 14:15:28 -0700

    fix(thunderbird): disable IPv6 to resolve Gmail SMTP timeouts

--- nixos/.config/nixos/modules/home/thunderbird.nix
+++ nixos/.config/nixos/modules/home/thunderbird.nix
@@ -6,6 +6,9 @@
         isDefault = true;
         # profileDir = "/home/matth/.thunderbird/"; # This is the default location
         # profileDir = "/home/matth/.mozilla-thunderbird/"; # This is the default location for some distributions
+        settings = {
+          "network.dns.disableIPv6" = true;
+        };
       }; # c8ym09yb
     };
   };
